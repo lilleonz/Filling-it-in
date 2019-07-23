52eab257d2f365ae20b024f04cb0db995fcd51d1
lilleonz <lilleonz@gmail.com>
2019-07-23 19:00:00 -0400

    Ch5 completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,4 @@
 class UsersController < ApplicationController
-  def index
-    @name = "I am the Index action!"
-  end
-
-  def contact
-    @name = "I am the Contact action!"
-  end
-
-  def about
-    @name = "I am the About action!"
-  end
-
-  def home
-    @name = "I am the Home action!"
+  def new
   end
 end
