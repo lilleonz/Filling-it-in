7b1d0c54d37d82e487fe96093bff335d9efd7a91
lilleonz <lilleonz@gmail.com>
2019-07-23 17:35:50 -0400

    added rails_default partial file

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,14 +4,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get home" do
     get root_path
-    get static_pages_home_url
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get help_path
-    get static_pages_help_url
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
