2096ff8a4a0f1e947bd9ab559f6ffd04b6d2417e
lilleonz <lilleonz@gmail.com>
2019-07-23 14:33:40 -0400

    added images and static pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  def index
+    @name = "I am the Index action!"
+  end
+
+  def contact
+    @name = "I am the Contact action!"
+  end
+
+  def about
+    @name = "I am the About action!"
+  end
+
+  def home
+    @name = "I am the Home action!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
   resources :users
+  resources :home
 end
